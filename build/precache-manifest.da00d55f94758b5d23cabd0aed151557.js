596bf5ea24a97fdc9dc8d5cf7c86cdcaf5a15508
Victoria Rogozhkina <v.rogozhkina@gmail.com>
2020-07-19 15:40:38 -0700

    Build

---
+++ build/precache-manifest.da00d55f94758b5d23cabd0aed151557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6817dfb9e683931e0f108d2d7c60cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b39ef0d1bf605f8777c",
+    "url": "/static/css/main.e80fe0ad.chunk.css"
+  },
+  {
+    "revision": "2ad97997d4d0424b3485",
+    "url": "/static/js/2.1d3de6bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d3de6bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b39ef0d1bf605f8777c",
+    "url": "/static/js/main.970d91d2.chunk.js"
+  },
+  {
+    "revision": "7954f395b78a0fc52086",
+    "url": "/static/js/runtime-main.f02aeaec.js"
+  }
+]);
